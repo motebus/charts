df572697b548b1bb19a0ebf364605680e3868d0b
YE,LI-WEI <a0970366492@gmail.com>
2020-04-29 16:13:25 +0800

    test

--- git.sh
+++ git.sh
@@ -1,4 +1,3 @@
-helm repo index .
 git add .
 git commit -m "$*"
 git push
--- package.sh
+++
@@ -1,7 +0,0 @@
-fbuilder-v=v1.2.0
-sscreen-v=v1.1.2
-helm package sscreen/ fbuilder/
-git checkout gh-pages
-sudo rm -rf fbuilder-v1.2.0.tgz
-sudo rm -rf sscreen-v1.1.2.tgz
-helm repo index .
